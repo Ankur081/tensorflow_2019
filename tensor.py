83ba1d73845d37f3c06bd04e8d0220536db25347
Ankur jain <45650353+Ankur081@users.noreply.github.com>
2019-03-09 09:31:49 +0530

    9-03-2019

---
+++ tensor.py
@@ -0,0 +1,11 @@
+import tensorflow as tf;
+
+graph =tf.Graph();
+with graph.as_default() :
+    x = tf.constant( 5, name="x_value");
+    y = tf.constant( 222, name="y_value");
+    sum = tf.add(x,y, name = "xy_sum");
+
+    with tf.Session() as session :
+        print("Result :" , sum.eval());
+
